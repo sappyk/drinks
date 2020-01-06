0097f4cee6ba2bd1c663b59e87bdd09e0dd75e53
sappyk <pavel_karih@mail.ru>
2020-01-06 20:25:50 +0800

    Update Menu.java

--- order/Menu.java
+++ order/Menu.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Menu {
-    
+
+    int coffee = 120;//стоимость кофе
+    int chocolate = 150;//стоимость шоколада
+
     private static int readNumber() {
         Scanner sc = new Scanner(System.in);
         int read;
@@ -52,6 +55,56 @@ public class Menu {
     
      public static void teaDrink() {
         System.out.println("Вы заказали чашечку чая!");
+            boolean start = true;
+            do {
+                System.out.println("Сахар, молоко добавим ? \n"
+                    +"1) Да\n"
+                    +"2) Нет\n"
+                    +"0) Я передумал(а)");
+                
+                switch (readNumber()) {
+                    case 0:
+                        start = false;
+                        System.out.println("До встречи!");
+                        break;
+                        
+                    case 1:
+                        addSmthToTea();
+                        break;
+                        
+                    case 2:
+                        costTea();
+                        break;
+                    
+                }
+            }while(start = false);
+    }
+     
+     private static void addSmthToTea() {
+            boolean start = true;
+            do {
+                System.out.println("Чего желаете ? \n"
+                    +"1) Молоко\n"
+                    +"2) Сахар\n"
+                    +"0) Я передумал(а)");
+                
+                switch (readNumber()) {
+                    case 0:
+                        start = false;
+                        System.out.println("До встречи!");
+                        break;
+                        
+                    case 1:
+                        milk();
+                        break;
+                        
+                    case 2:
+                        sugar();
+                        break;
+                    
+                }
+            }while(start = false);
+       
     }
      
      public static void coffeeDrink() {
@@ -61,7 +114,26 @@ public class Menu {
     public static void chocolateDrink() {
         System.out.println("Вы заказали чашечку шоколада!");
     }
-}
     
-
+    public static void milk() {
+        int milk = 20; //стоимость чая
+        int tea = 80;
+        int total = milk + tea;
+        System.out.println("С вас: " + total + " руб.");
         
+    }
+    
+    public static void sugar() {
+        int tea = 80; //стоимость чая
+        int sugar = 10;
+        int total = tea + sugar;
+        System.out.println("С вас: " + total + " руб.");
+        
+    }
+    
+    public static void costTea() {
+        int tea = 80; //стоимость чая
+        System.out.println("С вас: " + tea + " руб.");
+        
+    }
+}
